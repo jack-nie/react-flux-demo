0116d1c5630916fae91c12d898e0ee57a0b5b53b
jack-nie <hust.ud.hui@gmail.com>
2016-06-25 10:43:49 +0800

    update

--- src/js/actions/ButtonActions.js
+++ src/js/actions/ButtonActions.js
@@ -1,13 +1,13 @@
 import AppDispatcher from '../dispatcher/AppDispatcher';
 
 class ButtonActions {
-  
-  addNewItem = function (text) {
-    AppDispatcher.dispatch({
-      actionType: 'ADD_NEW_ITEM',
-      text: text
-    });
-  };
+
+    addNewItem = function(text) {
+        AppDispatcher.dispatch({
+            actionType: 'ADD_NEW_ITEM',
+            text: text
+        });
+    };
 
 }
 
