1e471f30d105538fb5e337a0570fdcf926f62615
jack-nie <hust.ud.hui@gmail.com>
2016-06-25 19:53:27 +0800

    fix code style

--- src/js/reducers/Reducer.js
+++ src/js/reducers/Reducer.js
@@ -2,13 +2,13 @@ import { createStore } from 'redux';
 import ListStore from '../stores/ListStore';
 
 const Reducer = function(state, action) {
-	switch(action.type) {
-		case 'ADD_NEW_ITEM':
-			ListStore.addNewItemHandler(action.text);
-		    ListStore.emitChange();
-	        break;
-	    default:
-	}
+  switch(action.type) {
+    case 'ADD_NEW_ITEM':
+      ListStore.addNewItemHandler(action.text);
+        ListStore.emitChange();
+          break;
+      default:
+  }
 }
 
 export default Reducer;
